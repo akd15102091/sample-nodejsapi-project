d3be2cd2a987246e324874ef9953cfe587a10bc4
Ashwini Dhaked <ashwini.dhaked@oracle.com>
2023-01-05 16:59:20 +0530

    sample api

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000 ;
 var bodyParser = require('body-parser');  
-const { response } = require("express");
 // Create application/x-www-form-urlencoded parser  
-var urlencodedParser = bodyParser.urlencoded({ extended: false })  
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req,res)=>{
     res.send("Hello World");
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {  
-    response = req.body;  
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(response));
-    res.end();
+app.post('/process_post', function (req, res) {  
+    let response = req.body; 
+    console.log("response is : ",response); 
+    res.send(JSON.stringify(response));
  })  
 
 app.listen(port, ()=> {
